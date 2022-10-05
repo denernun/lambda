25bdfcdf08d659a830fa4212ecf6f4baf3f57755
Dener Rocha <denernun@gmail.com>
2022-10-05 17:41:45 -0300

    .

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -6,8 +6,6 @@ export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
     // example resource
     // const queue = new sqs.Queue(this, 'LambdaQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
